7b332a2dde59dad168c8ba32bbd0db8aa02e738c
Jakub Slajer <j.slajer88@gmail.com>
2022-06-24 12:14:51 +0200

    test

--- com/core/settings.py
+++ com/core/settings.py
@@ -91,9 +91,6 @@ DATABASES = {
         'PASSWORD': os.environ.get("DB_PASSWORD"),
         'HOST': os.environ.get("DB_HOST"),
         'PORT': os.environ.get("DB_PORT"),
-        'TEST': {
-            'NAME': 'test_django-notes',
-        },
     },
 }
 

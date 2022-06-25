e3df2dddc5922693adf181bd3afe17ae232b96e5
Jakub Slajer <j.slajer88@gmail.com>
2022-06-25 18:01:55 +0200

    a

--- com/core/settings/base.py
+++ com/core/settings/base.py
@@ -87,7 +87,7 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get("DB_NAME"),
         'USER': os.environ.get("DB_USER"),
         'PASSWORD': os.environ.get("DB_PASSWORD"),
--- com/core/settings/test.py
+++ com/core/settings/test.py
@@ -14,6 +14,13 @@ if os.path.isfile(dotenv_file):
     dotenv.load_dotenv(dotenv_file, override=True)  # need to override default env values
 
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'test_database',
+    }
+}
+
 
 SECRET_TEST_EMAIL = os.getenv("SECRET_TEST_EMAIL")
 SECRET_TEST_PWD = os.environ.get("SECRET_TEST_PWD")
